0329b835086e3b620904af717999d3445522055c
Anna Gerchanovsky <annagerchanovsky@Annas-MacBook-Pro.local>
2023-03-10 18:50:41 -0500

    remove media type choices

--- web/KaraoKeySite/static/KaraoKeySite/record.js
+++ web/KaraoKeySite/static/KaraoKeySite/record.js
@@ -1,9 +1,11 @@
+// from https://github.com/Mido22/MediaRecorder-sample
+
 'use strict'
 
 let log = console.log.bind(console),
   id = val => document.getElementById(val),
   ul = id('ul'),
-  gUMbtn = id('gUMbtn'),
+  request_button = id('request_button'),
   start = id('start'),
   stop = id('stop'),
   stream,
@@ -12,36 +14,28 @@ let log = console.log.bind(console),
   chunks,
   media;
 
+request_button.onclick = e => {
+  media = {
+      tag: 'audio',
+      type: 'audio/ogg',
+      ext: '.ogg',
+      selector: {audio: true}
+    };
 
-gUMbtn.onclick = e => {
-  let mv = id('mediaVideo'),
-      mediaOptions = {
-        video: {
-          tag: 'video',
-          type: 'video/webm',
-          ext: '.mp4',
-          gUM: {video: true, audio: true}
-        },
-        audio: {
-          tag: 'audio',
-          type: 'audio/ogg',
-          ext: '.ogg',
-          gUM: {audio: true}
-        }
-      };
-  media = mv.checked ? mediaOptions.video : mediaOptions.audio;
-  navigator.mediaDevices.getUserMedia(media.gUM).then(_stream => {
+  navigator.mediaDevices.getUserMedia(media.selector).then(_stream => {
     stream = _stream;
-    id('gUMArea').style.display = 'none';
-    id('btns').style.display = 'inherit';
+    id('selection_area').style.display = 'none';
+    id('buttons_area').style.display = 'inherit';
     start.removeAttribute('disabled');
     recorder = new MediaRecorder(stream);
+    console.log(recorder);
     recorder.ondataavailable = e => {
       chunks.push(e.data);
       if(recorder.state == 'inactive')  makeLink();
     };
     log('got media successfully');
   }).catch(log);
+
 }
 
 start.onclick = e => {
@@ -53,26 +47,23 @@ start.onclick = e => {
 
 
 stop.onclick = e => {
+  console.log("stopped")
   stop.disabled = true;
   recorder.stop();
+  console.log("stopped2");
   start.removeAttribute('disabled');
 }
 
-
-
 function makeLink(){
+  console.log("making link");
   let blob = new Blob(chunks, {type: media.type })
     , url = URL.createObjectURL(blob)
     , li = document.createElement('li')
     , mt = document.createElement(media.tag)
-    , hf = document.createElement('a')
   ;
   mt.controls = true;
   mt.src = url;
-  hf.href = url;
-  hf.download = `${counter++}${media.ext}`;
-  hf.innerHTML = `donwload ${hf.download}`;
   li.appendChild(mt);
-  li.appendChild(hf);
+  ul.innerHTML = "";
   ul.appendChild(li);
 }
