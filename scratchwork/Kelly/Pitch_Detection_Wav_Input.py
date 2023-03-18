852c96d3f531a67b56abcc137366b696c8d8eae5
kwoicik <80640127+kwoicik@users.noreply.github.com>
2023-03-18 11:57:51 -0400

    Add new pitch tracker script with .wav file input
    Also included are the wav files recorded for testing and the .txt files of their pitch and confidence outputs.

---
+++ scratchwork/Kelly/Pitch_Detection_Wav_Input.py
@@ -0,0 +1,28 @@
+# aubio pitch detection on a wav file
+
+from aubio import source, pitch
+
+inputFile = "C_Major_Scale_Fast/C_Major_Scale_Fast_piano.wav"
+
+samplerate = 44100
+frames = 1024
+fftSize = 2048
+
+s = source(inputFile, samplerate, frames)
+samplerate = s.samplerate
+
+tolerance = 0.8
+
+aubioPitch = pitch("yinfft", fftSize, frames, samplerate)
+aubioPitch.set_unit("Hz")
+aubioPitch.set_tolerance(tolerance)
+
+# total number of frames read
+total_frames = 0
+while True:
+    samples, read = s()
+    pitch = aubioPitch(samples)[0]
+    confidence = aubioPitch.get_confidence()
+    print(f"{total_frames/float(pitch, confidence)} {pitch} {confidence}")
+    total_frames += read
+    if read < frames: break
