d302e0424a72cc241abcbf2d296f8893223d8d07
azhiyima517 <anitama@andrew.cmu.edu>
2023-04-02 18:55:30 -0400

    modularized code integrated note detection to pda

--- scratchwork/Anita/feedback_wav_interface/pitch_detection_wav_input.py
+++ scratchwork/Anita/feedback_wav_interface/pitch_detection_wav_input.py
@@ -1,4 +1,14 @@
-# aubio pitch detection on a wav file
+################################################################################
+# pitch_detection_wav_input.py
+#
+# Basic proof of concept of the aubio pitch detection algorithm combined with 
+# the note detection algorithm. 
+# 
+# Choose the input file to detect frequencies from by modifying the file 
+# stored in "inputFile." Then run the code-- output of both the pitch and the 
+# note data, consisting of note, octave, and deviation in cents, will be printed
+# to the terminal 
+################################################################################
 
 from aubio import source, pitch
 from feedback import *
@@ -25,7 +35,7 @@ while True:
     pitch = aubioPitch(samples)[0]
     confidence = aubioPitch.get_confidence()
     if (pitch != 0):
-        note, octave, _ = frequency_to_note_data(pitch)
-        print(f"{total_frames/float(samplerate)} {pitch}: {note}{octave}")
+        note, octave, dev = frequency_to_note_data(pitch)
+        print(f"{total_frames/float(samplerate)} {pitch}: {note}{octave}, deviation (cents):{dev}")
     total_frames += read
     if read < frames: break
